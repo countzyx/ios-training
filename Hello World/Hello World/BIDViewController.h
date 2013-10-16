5440d4d30b123d3925651ac2e9efff693d8936c3
Pedro R. Jiménez <ptpedroj@gmail.com>
2013-10-16 05:55:15 -0400

    Initial Check-In of Hello World App
    This is the Chapter 2 app from the Beginning iOS6 Development book from
    Apress. This is the stuff created with the Single View project template,
    unchanged.

---
+++ Hello World/Hello World/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Hello World
+//
+//  Created by Pedro Jimenez on 10/16/13.
+//  Copyright (c) 2013 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
