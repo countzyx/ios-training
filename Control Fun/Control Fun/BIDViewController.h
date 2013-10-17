1c0714ae1ee19a7c8f753871e9e5832c9c9d740a
Pedro R. Jiménez <ptpedroj@gmail.com>
2013-10-17 10:18:54 -0400

    Starting to Add UI Elements
    Added an image view, two labels and two text fields. The keyboard won't
    go away once you select a text field. Need to fix that next.

---
+++ Control Fun/Control Fun/BIDViewController.h
@@ -0,0 +1,15 @@
+//
+//  BIDViewController.h
+//  Control Fun
+//
+//  Created by Pedro Jimenez on 10/17/13.
+//  Copyright (c) 2013 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *numberField;
+
+@end
