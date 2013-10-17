f6e4e883ac233356734c41a45289fb09bd40c812
Pedro R. Jiménez <ptpedroj@gmail.com>
2013-10-16 21:55:55 -0400

    Initial Check-In of Chapter 3 Button Fun App
    This is the project for the Chapter 3 app in the Beginning iOS 6
    Development book from Apress. Let's get started.

---
+++ Button Fun/Button Fun/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Button Fun
+//
+//  Created by Pedro Jimenez on 10/16/13.
+//  Copyright (c) 2013 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Button Fun/Button Fun/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Button Fun
+//
+//  Created by Pedro Jimenez on 10/16/13.
+//  Copyright (c) 2013 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
