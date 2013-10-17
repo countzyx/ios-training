6a512593a1966ba3fcbe2fede75d5cc4c22ed971
Pedro R. Jiménez <ptpedroj@gmail.com>
2013-10-17 00:05:49 -0400

    Changed the Label to Show Formatted Text
    The label now shows the name of the button pressed in bold. This is an
    intro into NSAttributedString.

--- Button Fun/Button Fun/BIDViewController.m
+++ Button Fun/Button Fun/BIDViewController.m
@@ -17,6 +17,16 @@
 - (IBAction)buttonPressed:(UIButton *)sender {
 	NSString *title = [sender titleForState:UIControlStateNormal];
 	NSString *plainText = [NSString stringWithFormat:@"%@ button pressed.", title];
-	_statusLabel.text = plainText;
+	
+	NSMutableAttributedString *styledText = [[NSMutableAttributedString alloc] initWithString:plainText];
+	
+	NSDictionary *attributes = @{
+		NSFontAttributeName : [UIFont boldSystemFontOfSize:_statusLabel.font.pointSize]
+	};
+	
+	NSRange nameRange = [plainText rangeOfString:title];
+	
+	[styledText setAttributes:attributes range:nameRange];
+	_statusLabel.attributedText = styledText;
 }
 @end
